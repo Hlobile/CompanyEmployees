25c1a016cf0c0df04abca31d94f697e96f3b8b6a
Hlobile <62103682+Hlobile@users.noreply.github.com>
2021-02-08 23:13:37 +0200

    Added db context and migrations, also updated database

--- CompanyEmployees/Extensions/ServiceExtensions.cs
+++ CompanyEmployees/Extensions/ServiceExtensions.cs
@@ -1,6 +1,9 @@
 ﻿using Contracts;
+using Entities;
 using LoggerServices;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
 using System;
@@ -30,5 +33,12 @@ namespace CompanyEmployees.Extensions
 
         public static void ConfigureLoggerService(this IServiceCollection services) =>
             services.AddScoped<ILoggerManager, LoggerManager>();
-}
+
+
+        public static void ConfigureSqlContext(this IServiceCollection services,
+            IConfiguration configuration) =>
+            services.AddDbContext<RepositoryContext>(opts =>
+            opts.UseSqlServer(configuration.GetConnectionString("sqlConnection"), b =>
+            b.MigrationsAssembly("CompanyEmployees")));
+    }
 }
